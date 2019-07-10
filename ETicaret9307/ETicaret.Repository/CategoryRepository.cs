dfb5b97f3c3b8d72577d7c603c3609f123e936d1
d.selici <d.selici@gmail.com>
2019-07-10 20:55:16 +0300

    DataRepository ve CategoryRepository eklendi.

---
+++ ETicaret9307/ETicaret.Repository/CategoryRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eticaret.Common;
+using ETicaret.Entity;
+
+namespace ETicaret.Repository
+{
+    class CategoryRepository : DataRepository<Category, Guid>
+    {
+        static ECommerceEntities db = Tool.GetConnection();
+        ResultProcess<Category> result = new ResultProcess<Category>();
+
+        public override Result<int> Delete(Guid id)
+        {
+            Category c = db.Categories.SingleOrDefault(t => t.CategoryId == id);
+            db.Categories.Remove(c);
+            return result.GetResult(db);
+        }
+
+        public override Result<Category> GetObjById(Guid id)
+        {
+            Category c = db.Categories.SingleOrDefault(t => t.CategoryId == id);
+            return result.GetT(c);
+        }
+
+        public override Result<int> Insert(Category item)
+        {
+            Category yeni = db.Categories.Create();
+            yeni.CategoryName = item.CategoryName;
+            yeni.CreatedDate = DateTime.Now;
+            yeni.Description = item.Description;
+
+            db.Categories.Add(yeni);
+            return result.GetResult(db);
+            
+        }
+
+        public override Result<List<Category>> List()
+        {
+            List<Category> CatList = db.Categories.ToList();
+            return result.GetListResult(CatList);
+        }
+
+        public override Result<int> Update(Category item)
+        {
+            Category dbdeki = db.Categories.SingleOrDefault(t => t.CategoryId == item.CategoryId);
+
+            dbdeki.CategoryName = item.CategoryName;
+            dbdeki.Description = item.Description;
+
+            return result.GetResult(db);
+        }
+    }
+}
