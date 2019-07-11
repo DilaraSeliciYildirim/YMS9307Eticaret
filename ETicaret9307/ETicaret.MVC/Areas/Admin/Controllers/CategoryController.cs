bb92dda822f8118461636c35ce6fd4cfd1758131
d.selici <d.selici@gmail.com>
2019-07-11 20:00:35 +0300

    Admin Layout'u oluşturuldu.

---
+++ ETicaret9307/ETicaret.MVC/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ETicaret.MVC.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Admin/Category
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
