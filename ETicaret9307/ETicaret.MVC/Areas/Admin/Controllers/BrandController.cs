505d1b0981f9e2ae2f54f23fca25647d53e20c8c
d.selici <d.selici@gmail.com>
2019-07-16 21:17:12 +0300

    Brand ekleme yapıldı

---
+++ ETicaret9307/ETicaret.MVC/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ETicaret.MVC.Areas.Admin.Models.ResultModel;
+using ETicaret.Entity;
+using ETicaret.Repository;
+
+namespace ETicaret.MVC.Areas.Admin.Controllers
+{
+    public class BrandController : Controller
+    {
+        // GET: Admin/Brand
+        BrandRepository br = new BrandRepository();
+        InstanceResult<Brand> result = new InstanceResult<Brand>();
+        public ActionResult BrandList()
+        {
+            result.resultList = br.List();
+            return View(result.resultList.ProcessResult);
+        }
+
+        [HttpGet]
+        public ActionResult AddBrand()
+        {
+            return View();
+        }
+
+        public ActionResult AddBrand(Brand model, HttpPostedFileBase file)
+        {
+            string PhotoName = ""; //Resmin adı
+            if (file !=null && file.ContentLength>0)
+            {
+                PhotoName = Guid.NewGuid().ToString().Replace("-", "") + ".jpg";
+                string path = Server.MapPath("~/Images/" + PhotoName); //Resmin kaydolacağı yer.
+                file.SaveAs(path); //Resmi belirtilen path'e kaydettik.
+            }
+
+            model.Photo = PhotoName; //model'e path'i değil PhotoName'i veriyoruz li path üzerinde bir değişiklik yapılmak istenirse işlem zorlaşmasın
+            result.resultint = br.Insert(model);
+
+            if (result.resultint.IsSucceeded)
+            {
+                return RedirectToAction("BrandList");
+            }
+            else
+            {
+                ViewBag.hata = result.resultint.UserMessage;
+                return RedirectToAction("AddBrand");
+            }
+        }
+    }
+}
---
+++ ETicaret9307/ETicaret.Repository/BrandRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eticaret.Common;
+using ETicaret.Entity;
+
+namespace ETicaret.Repository
+{
+    public class BrandRepository : DataRepository<Brand, int>
+    {
+        public static ECommerceEntities db = Tool.GetConnection();
+        ResultProcess<Brand> result = new ResultProcess<Brand>();
+        public override Result<int> Delete(int id)
+        {
+            Brand silinecek = db.Brands.SingleOrDefault(t => t.BrandId == id);
+            db.Brands.Remove(silinecek);
+            return result.GetResult(db);
+        }
+
+        public override Result<Brand> GetObjById(int id)
+        {
+            Brand brand = db.Brands.SingleOrDefault(t => t.BrandId == id);
+            return result.GetT(brand);
+        }
+
+        public override Result<int> Insert(Brand item)
+        {
+            Brand yeni = db.Brands.Create();
+            yeni.BrandName = item.BrandName;
+            yeni.Description = item.Description;
+            yeni.Photo = item.Photo;
+
+            db.Brands.Add(yeni);
+
+            return result.GetResult(db);
+        }
+
+        public override Result<List<Brand>> List()
+        {
+            return result.GetListResult(db.Brands.ToList());
+        }
+
+        public override Result<int> Update(Brand item)
+        {
+            Brand guncellenecek = db.Brands.SingleOrDefault(t => t.BrandId == item.BrandId);
+            guncellenecek.BrandName = item.BrandName;
+            guncellenecek.Description = item.Description;
+            guncellenecek.Photo = item.Photo;
+
+            return result.GetResult(db);
+        }
+    }
+}
