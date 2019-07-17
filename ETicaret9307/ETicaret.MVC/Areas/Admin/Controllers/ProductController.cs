fad2ade533cf57a25362937d5a99f138ee6081a9
d.selici <d.selici@gmail.com>
2019-07-17 21:15:05 +0300

    ProductList, ProductController'a eklendi

---
+++ ETicaret9307/ETicaret.MVC/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ETicaret.Entity;
+using ETicaret.Repository;
+using ETicaret.MVC.Areas.Admin.Models.ResultModel;
+
+namespace ETicaret.MVC.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Admin/Product
+
+        ProductRepository pr = new ProductRepository();
+        InstanceResult<Product> result = new InstanceResult<Product>();
+        public ActionResult ProductList()
+        {
+            result.resultList = pr.List();
+            return View(result.resultList.ProcessResult);
+        }
+    }
+}
