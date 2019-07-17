3fe985bd8ffec12f435c0ab58852b2fe53d44d9b
d.selici <d.selici@gmail.com>
2019-07-17 20:34:01 +0300

    BrandConroller Completed

--- ETicaret9307/ETicaret.MVC/Areas/Admin/Controllers/BrandController.cs
+++ ETicaret9307/ETicaret.MVC/Areas/Admin/Controllers/BrandController.cs
@@ -14,9 +14,13 @@ namespace ETicaret.MVC.Areas.Admin.Controllers
         // GET: Admin/Brand
         BrandRepository br = new BrandRepository();
         InstanceResult<Brand> result = new InstanceResult<Brand>();
-        public ActionResult BrandList()
+        public ActionResult BrandList(string mesaj)
         {
             result.resultList = br.List();
+            if (mesaj !=null)
+            {
+            ViewBag.deleteMesaj = String.Format("Silme işlemi {0}", mesaj);
+            }
             return View(result.resultList.ProcessResult);
         }
 
@@ -29,6 +33,7 @@ namespace ETicaret.MVC.Areas.Admin.Controllers
         public ActionResult AddBrand(Brand model, HttpPostedFileBase file)
         {
             string PhotoName = ""; //Resmin adı
+
             if (file !=null && file.ContentLength>0)
             {
                 PhotoName = Guid.NewGuid().ToString().Replace("-", "") + ".jpg";
@@ -49,5 +54,45 @@ namespace ETicaret.MVC.Areas.Admin.Controllers
                 return RedirectToAction("AddBrand");
             }
         }
+
+        [HttpGet]
+        public ActionResult EditBrand(int id)
+        {
+            result.resultT = br.GetObjById(id);
+            return View(result.resultT.ProcessResult);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult EditBrand(Brand model, HttpPostedFileBase Photo)
+        {
+            string PhotoName = model.Photo; //Model'in photosunu hidden input olarak göndermezsek Photo property'si null olacaktır. Kullanıcı fotoğrafı değiştirmek istemezse hataya sebep olacaktır.
+
+            if (Photo !=null && Photo.ContentLength>0)
+            {
+                PhotoName = Guid.NewGuid().ToString().Replace("-", "") + ".jpg";
+                string path = Server.MapPath("~/Images/" + PhotoName);
+                Photo.SaveAs(path);
+            }
+
+            model.Photo = PhotoName;
+            result.resultint = br.Update(model);
+            if (result.resultint.IsSucceeded)
+            {
+                return RedirectToAction("BrandList");
+            }
+            else
+            {
+                return RedirectToAction("EditBrand", new { @id = model.BrandId });
+               // return View(model);
+            }
+
+        }
+
+        public ActionResult DeleteBrand(int id)
+        {
+            result.resultint = br.Delete(id);
+            return RedirectToAction("BrandList", new { @mesaj = result.resultint.UserMessage });
+        }
+
     }
 }
