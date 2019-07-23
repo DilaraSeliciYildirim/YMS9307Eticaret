9d05e17fbaf7af7ca4f39b09895a1dc959afb562
d.selici <d.selici@gmail.com>
2019-07-23 20:48:08 +0300

    EditMember ve DeleteMember eklendi, MemberControler completed

--- ETicaret9307/ETicaret.MVC/Areas/Admin/Controllers/MemberController.cs
+++ ETicaret9307/ETicaret.MVC/Areas/Admin/Controllers/MemberController.cs
@@ -14,9 +14,15 @@ namespace ETicaret.MVC.Areas.Admin.Controllers
         // GET: Admin/Member
         MemberRepository mr = new MemberRepository();
         InstanceResult<Member> result = new InstanceResult<Member>();
-        public ActionResult MemberList()
+        public ActionResult MemberList(string M, int? ID)
         {
+            //int? nullable olmalıdır çünkü null olmazsa silme işlemi değil de listeyi açma işlemi yapılıyorsa parametreler null gelecektir. Bu durum int null olamayacağı exception fırlatır.
             result.resultList = mr.List();
+
+            if (M!=null && ID!=null) // silme işlemi yapılıyor
+            {
+                ViewBag.SilmeMesaji = String.Format("{0} id'li kullanıcının silme işlemi {1}", ID, M);
+            }
             return View(result.resultList.ProcessResult);
         }
 
@@ -58,6 +64,48 @@ namespace ETicaret.MVC.Areas.Admin.Controllers
             {
                 return RedirectToAction("AddMember", new { @mesajUye = "Bu maille kayıtlı bir admin bulunmaktadır" });
             }
+        }
+
+        [HttpGet]
+        public ActionResult EditMember(int id)
+        {
+            return View(mr.GetObjById(id).ProcessResult);
+        }
+
+        [HttpPost]
+        public ActionResult EditMember(Member model, string confirm, string oldPassword, string newPassword)
+        {
+            if (model.Password == oldPassword)
+            {
+                if (newPassword == confirm)
+                {
+                    model.Password = newPassword;
+                    result.resultint = mr.Update(model);
+                    if (result.resultint.IsSucceeded)
+                    {
+                        return RedirectToAction("MemberList");
+                    }
+
+                    return RedirectToAction("EditMember", new { @id = model.UserId });
+                }
+                else
+                {
+                    ViewBag.HataPassword = "Parolalar uyuşmuyor";
+                    return View(model);
+                }
+            }
+            else
+            {
+                ViewBag.OldPasswordWrong = "Eski parola yanlış girildi";
+                return View(model);
+            }
+         
+        }
+
+        public ActionResult DeleteMember(int id)
+        {
+            result.resultint = mr.Delete(id);
+            return RedirectToAction("MemberList", new { @ID = id, @M = result.resultint.UserMessage });
         }
     }
 }
