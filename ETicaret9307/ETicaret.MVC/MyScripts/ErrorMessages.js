db2f66de014260e1560928cf6c38e26fa95a1fd7
d.selici <d.selici@gmail.com>
2019-08-01 20:40:08 +0300

    Jquery validation eklendi.

---
+++ ETicaret9307/ETicaret.MVC/MyScripts/ErrorMessages.js
@@ -0,0 +1,8 @@
+﻿jQuery.extend(jQuery.validator.messages, {
+
+    required: "Bu alan doldurulmalı!",
+    email: "Geçerli bir mail adresi giriniz",
+    equalTo: "Aynı değeri giriniz",
+    rangelength: jQuery.validator.format("{0} ile {1} karakter uzunluğunda değer giriniz")
+
+});
---
+++ ETicaret9307/ETicaret.MVC/MyScripts/main.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+
+    $("#js-add-member-form").validate({
+
+        rules: {
+            FirstName: "required",
+            LastName:
+            {
+                required: true
+       
+            },
+            Email: {
+                required: true,
+                email:true
+            },
+            Password: {
+                required: true,
+                rangelength: [8,16]
+            },
+            confirm: {
+                equalTo:"#js-input-password"
+            }
+
+        }
+
+
+    });
+
+});
